990ed97764d2feca1420ac6db5d500ad8e041d6d
Jivak Sute <jivak.sute@rxcs.in>
2025-04-24 16:02:52 +0530

    AgriGo Project Setup and user authentication.

---
+++ Backend/index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const { connection } = require("./config/db");
+const { userRouter } = require("./routes/userRouter");
+const app = express();
+require("dotenv").config();
+
+app.use(express.json());
+
+// Basic test route
+app.get("/", (req, res) => {
+  res.send("Welcome to agriGo.");
+});
+
+
+app.use("/auth/user", userRouter);
+
+// Start server
+app.listen(process.env.PORT, async () => {
+  try {
+    await connection;
+    console.log("Connected to the Database");
+  } catch (error) {
+    console.error("Database connection failed:", error.message);
+  }
+  console.log(`Server is running on port ${process.env.PORT}`);
+});
